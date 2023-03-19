045076b8cd9852ed5ee7710b6031f56e04acb475
viviio <120554829+viviio@users.noreply.github.com>
2023-03-19 22:52:55 +0800

    Update config_medium.js

--- themes/medium/config_medium.js
+++ themes/medium/config_medium.js
@@ -4,7 +4,7 @@ const CONFIG_MEDIUM = {
   RIGHT_PANEL_DARK: process.env.NEXT_PUBLIC_MEDIUM_RIGHT_DARK || false, // 右侧面板深色模式
 
   POST_LIST_COVER: true, // 文章列表显示图片封面
-  POST_LIST_PREVIEW: true, // 列表显示文章预览
+  POST_LIST_PREVIEW: false, // 列表显示文章预览
   POST_LIST_CATEGORY: true, // 列表显示文章分类
   POST_LIST_TAG: true, // 列表显示文章标签
 
@@ -12,10 +12,10 @@ const CONFIG_MEDIUM = {
   POST_DETAIL_TAG: true, // 文章显示标签
 
   // 菜单
-  MENU_CATEGORY: true, // 显示分类
-  MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: true, // 显示归档
-  MENU_SEARCH: true, // 显示搜索
+  MENU_CATEGORY: false, // 显示分类
+  MENU_TAG: false, // 显示标签
+  MENU_ARCHIVE: false, // 显示归档
+  MENU_SEARCH: false, // 显示搜索
 
   // Widget
   WIDGET_REVOLVER_MAPS: process.env.NEXT_PUBLIC_WIDGET_REVOLVER_MAPS || 'false', // 地图插件
